2c333a8511e6f28b6f561ef2728d164965efeb58
AnthonyTomas2011 <145863980+Jay-472013@users.noreply.github.com>
2024-01-02 18:50:01 -0600

    ABCDEFG
    blah blah

--- JPSketchHead/Assets/Scripts/GameController.cs
+++ JPSketchHead/Assets/Scripts/GameController.cs
@@ -5,15 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform;
+    //Default position for platform
+    float pos = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Integer i equals 1000
+        for(int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnPlatforms();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    //Spawn platforms function
+    void SpawnPlatforms()
+    {
+        //Spawn platforms randomly on the x axis and place them on the Y axis every 2.5
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
     }
 }
